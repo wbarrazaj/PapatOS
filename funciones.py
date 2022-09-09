9d04c91ab95d46c8892ca52a9f2a69dd3a5d2acf
wbarrazaj <wbarraza@gmail.com>
2022-09-09 16:39:29 -0300

    Funciones

---
+++ funciones.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import psutil
+import time
+from datetime import datetime, timedelta
+
+
+def bytes2human(n, format="%(value).1f%(symbol)s"):
+    """Used by various scripts. See:
+    http://goo.gl/zeJZl
+
+    >>> bytes2human(10000)
+    '9.8K'
+    >>> bytes2human(100001221)
+    '95.4M'
+    """
+    symbols = ('B', 'K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    prefix = {}
+    for i, s in enumerate(symbols[1:]):
+        prefix[s] = 1 << (i + 1) * 10
+    for symbol in reversed(symbols[1:]):
+        if n >= prefix[symbol]:
+            value = float(n) / prefix[symbol]
+            return format % locals()
+    return format % dict(symbol=symbols[0], value=n)
+
+
+def print_(msg):
+    print(msg)
+
+
+def printlog(prompt):
+    year, mon, mday, hour, min, sec, wday, yday, isdst = time.localtime()
+    print("%04d-%02d-%02d %02d:%02d:%02d %s" % (year, mon , mday, hour, min, 
+sec, prompt))
+
+
+print_("Hola")
+
+printlog("Error 111")
+
+print(bytes2human(1024*1024*1024*1024*1024))
