4413643a77000a097871d99c6f12217952a1611a
wbarrazaj <wbarraza@gmail.com>
2022-09-12 01:02:08 -0300

    .

--- clases/clases.py
+++ clases/clases.py
@@ -103,3 +103,22 @@ class Server:
         disk_io = psutil.disk_io_counters()
         print(f"Total read: {get_size(disk_io.read_bytes)}")
         print(f"Total write: {get_size(disk_io.write_bytes)}")
+
+    def uso_Red (self):
+        #INFORMACIÓN DE REDES
+        print("="*40, "Network Information", "="*40)
+        if_addrs = psutil.net_if_addrs()
+        for interface_name, interface_addresses in if_addrs.items():
+            for address in interface_addresses:
+                print(f"=== Interface: {interface_name} ===")
+                if str(address.family) == 'AddressFamily.AF_INET':
+                    print(f"  IP Address: {address.address}")
+                    print(f"  Netmask: {address.netmask}")
+                    print(f"  Broadcast IP: {address.broadcast}")
+                elif str(address.family) == 'AddressFamily.AF_PACKET':
+                    print(f"  MAC Address: {address.address}")
+                    print(f"  Netmask: {address.netmask}")
+                    print(f"  Broadcast MAC: {address.broadcast}")
+        net_io = psutil.net_io_counters()
+        print(f"Total Bytes Sent: {get_size(net_io.bytes_sent)}")
+        print(f"Total Bytes Received: {get_size(net_io.bytes_recv)}")
