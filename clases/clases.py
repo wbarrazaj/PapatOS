960813cc62a0ec7accf4f7e57170475a5782d5b9
wbarrazaj <wbarraza@gmail.com>
2022-09-12 00:42:27 -0300

    .

--- clases/clases.py
+++ clases/clases.py
@@ -70,17 +70,17 @@ class Server:
         pass
     
     def uso_Memoria (self):
-        print("="*40, "Memory Information", "="*40)
+        print("="*20, "Memory Information", "="*20)
         svmem = psutil.virtual_memory()
-        print(f"Total: {get_size(svmem.total)}")
-        print(f"Available: {get_size(svmem.available)}")
-        print(f"Used: {get_size(svmem.used)}")
-        print(f"Percentage: {svmem.percent}%")
+        print(f"Total               : {bytes2human(self.Memory_Total)}")
+        print(f"Available           : {bytes2human(svmem.available)}")
+        print(f"Used                : {bytes2human(svmem.used)}")
+        print(f"Percentage          : {svmem.percent}%")
         print("="*20, "SWAP", "="*20)
         swap = psutil.swap_memory()
-        print(f"Total: {get_size(swap.total)}")
-        print(f"Free: {get_size(swap.free)}")
-        print(f"Used: {get_size(swap.used)}")
-        print(f"Percentage: {swap.percent}%")
+        print(f"Total               : {bytes2human(self.Swap_Total)}")
+        print(f"Free                : {bytes2human(swap.free)}")
+        print(f"Used                : {bytes2human(swap.used)}")
+        print(f"Percentage          : {swap.percent}%")
 
 
