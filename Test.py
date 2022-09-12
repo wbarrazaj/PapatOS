c42815fb613b8859a0d378855dfb7653cb5d05c5
wbarrazaj <wbarraza@gmail.com>
2022-09-11 22:52:41 -0300

    Clases

---
+++ Test.py
@@ -0,0 +1,10 @@
+from http import server
+from modulos.funciones import print_ , bytes2human , printlog
+from clases.clases import Server
+#import platform
+
+Servidor = Server()
+
+Servidor.print_Server()
+
+
--- clases/clases.py
+++ clases/clases.py
@@ -1,6 +1,6 @@
 import os
 import psutil
-
+import platform
 class Procesos:
     pass
 
@@ -20,4 +20,31 @@ class Employee:
     pass
 
 class Server:
-    pass
+    """
+    Server class Imprime infornacion de Servidor.
+    print_Server    : Imprime informcion de Servidor.
+        Systen      : Sistema Operativo.
+        Node_Name   : 
+        Release     : Release del Sistema Operativo.
+        Version     : Version del Sistema Operativo.
+        Machine     : Maquina.
+        Processor   : Procesador.
+    """
+
+    def __init__(self):
+        uname = platform.uname()
+        self.System=uname.system
+        self.Node_Name=uname.node
+        self.Release=uname.release
+        self.Version=uname.version
+        self.Machine=uname.machine
+        self.Processor=uname.processor
+
+    def print_Server(self):
+        print(f"System: {self.System}")
+        print(f"Node Name: {self.Node_Name}")
+        print(f"Release: {self.Release}")
+        print(f"Version: {self.Version}")
+        print(f"Machine: {self.Machine}")
+        print(f"Processor: {self.Processor}")
+
