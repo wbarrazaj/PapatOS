9bc5efc394baf77702ebfb12edbb71682a1b39b9
wbarrazaj <wbarraza@gmail.com>
2022-09-13 15:36:38 -0300

    .

--- barra_progreso.py
+++ barra_progreso.py
@@ -4,7 +4,13 @@
 import time
 import sys
 
+"""
 for i in range(100):
-    time.sleep(1)
+    time.sleep(0.1)
     sys.stdout.write("r%d%%" % i)
     sys.stdout.flush()
+"""
+
+from tqdm import tqdm
+for i in tqdm(range(100),ascii=True):
+    time.sleep(0)
