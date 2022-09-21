4e23a862e29ae069fac5379ee10a6abe9d646e3d
wbarrazaj <wbarraza@gmail.com>
2022-09-21 00:07:22 -0300

    pstree

--- pstree.py
+++ pstree.py
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2009, Giampaolo Rodola'. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
+# found in the LICENSE file
 
 """
 Similar to 'ps aux --forest' on Linux, prints the process list
